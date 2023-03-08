554c0c647acb63eb5a740dd31286b4981d0c40ca
huynhvantamm <huynhvantamm@gmail.com>
2023-03-08 08:56:30 +0700

    header-layout-F

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,72 +3,86 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-blue-600  grid grid-cols-12 px-[30px] xl:px-[100px]">
+      <nav className="bg-blue-600  grid grid-cols-12 px-[30px] 2xl:px-[100px]">
         {/* ----------===logo===---------- */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
         />
-        <div className="bg-blue-200 col-start-1 col-span-6 xl:col-start-1 xl:col-span-3 px-[15px] py-[20px]">
+        <div
+          className="col-start-1 col-span-6  lg:col-start-1 lg:col-span-3 xl:col-start-1 xl:col-span-3
+        bg-blue-200 px-[15px] py-[20px]
+        "
+        >
           <img
             src="https://ordainit.com/Sorex/sorex/assets/img/logo/logo-1.png"
             alt=""
           />
         </div>
         {/* ----------===nav===---------- */}
-        <div className="hidden xl:block xl:bg-red-200  xl:col-start-4 xl:col-span-6">
-          <nav className=" bg-red-200  col-start-4 col-span-6 px-[15px] flex justify-center font-semibold ">
-            <ul className=" bg-red-100 flex justify-end items-center w-full text-[17px]">
-              <li className="pl-[26px] py-[35px] bg-red-200 ">
+        <div className="hidden lg:block xl:bg-red-200  xl:col-start-4 xl:col-span-6 lg:col-start-4 lg:col-span-7">
+          <nav
+            className=" col-start-4 col-span-6
+           bg-red-200  xl:px-[15px] flex justify-center font-semibold "
+          >
+            <ul className=" bg-red-100 flex lg:justify-center xl:justify-end items-center w-full text-[17px]">
+              <li className="lg:px-[14px] xl:pl-[16px] py-[35px] bg-red-200 ">
                 <a href="/" className="">
-                  Home
+                  Home&nbsp;
                 </a>
-                <i className="ml-[10px] fas fa-angle-double-down "></i>
+                <i className=" fas fa-angle-double-down "></i>
                 <ul className="hidden">
-                  <li className="pl-[26px] bg-red-200">
+                  <li className="lg:px-[14px] xl:pl-[16px] bg-red-200">
                     <a href="">Home 1</a>
                   </li>
-                  <li className="pl-[26px] bg-red-200">
+                  <li className="lg:px-[14px] xl:pl-[16px] bg-red-200">
                     <a href="">Home 1</a>
                   </li>
-                  <li className="pl-[26px]  bg-red-200">
+                  <li className="lg:px-[14px] xl:pl-[16px]  bg-red-200">
                     <a href="">Home 1</a>
                   </li>
                 </ul>
               </li>
-              <li className="pl-[26px] py-[35px]  bg-red-200">
-                <a href="/about">About</a>
-                <i className="ml-[10px]  fas fa-angle-double-down"></i>
+              <li className="lg:px-[14px] xl:pl-[16px] py-[35px]  bg-red-200">
+                <a href="/about">About&nbsp;</a>
+                <i className="  fas fa-angle-double-down"></i>
               </li>
-              <li className="pl-[26px] py-[35px] bg-red-200">
-                <a href="/services">Services</a>
-                <i className="ml-[10px]  fas fa-angle-double-down"></i>
+              <li className="lg:px-[14px] xl:pl-[16px] py-[35px] bg-red-200">
+                <a href="/services">Services&nbsp;</a>
+                <i className="  fas fa-angle-double-down"></i>
               </li>
-              <li className="pl-[26px] py-[35px] bg-red-200">
-                <a href="/contact">Page</a>
-                <i className="ml-[10px] fas fa-angle-double-down"></i>
+              <li className="lg:px-[14px] xl:pl-[16px] py-[35px] bg-red-200">
+                <a href="/contact">Page&nbsp;</a>
+                <i className=" fas fa-angle-double-down"></i>
               </li>
-              <li className="pl-[26px] py-[35px] bg-red-200">
-                Blog<i className="ml-[10px] fas fa-angle-double-down"></i>
+              <li className="lg:px-[14px] xl:pl-[16px] py-[35px] bg-red-200">
+                Blog&nbsp;<i className=" fas fa-angle-double-down"></i>
               </li>
-              <li className="pl-[26px] py-[35px] bg-red-200">Contact</li>
+              <li className="lg:px-[14px] xl:pl-[16px] py-[35px] bg-red-200">
+                Contact
+              </li>
             </ul>
           </nav>
         </div>
         {/* ----------===menu===---------- */}
-        <div className=" col-start-10 col-span-3 bg-red-300  relative px-[15px]">
-          <button className=" bg-yellow-300 rounded-3xl font-semibold h-[52px] w-[135px] text-gray-900 absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] ">
+        <div
+          className="col-start-7 col-span-6 xl:col-start-10 xl:col-span-3 lg:col-start-11 lg:col-span-2
+          relative bg-red-300  px-[15px] flex justify-between items-center"
+        >
+          <button
+            className=" hidden xl:inline-block 
+            absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%]
+           bg-yellow-300 rounded-3xl font-semibold h-[52px] w-[135px] text-gray-900   "
+          >
             Get A Quote
           </button>
-          <button className="bg-gray-700 h-[52px] w-[52px] absolute top-2/4 left-full translate-x-[-100%] translate-y-[-50%] ">
-            <i className=" fas fa-bars  text-yellow-500 border-[12px] border-gray-700 rounded "></i>
+          <button
+            className="absolute top-2/4 left-full translate-x-[-125%] translate-y-[-50%]
+           bg-gray-700 h-[52px] w-[52px]  "
+          >
+            <i className="fas fa-bars text-yellow-500 border-[12px] border-gray-700 rounded "></i>
           </button>
         </div>
-        {/* <div className="flex bg-blue-300  col-start-7 col-span-6 xl:col-start-10 xl:col-span-3 relative text-center  justify-end items-center p-[15px]">
-          <button className="bg-gray-700 h-[52px] w-[52px] absolute  ">
-            <i className=" fas fa-bars  text-yellow-500 border-[12px] border-gray-700 rounded "></i>
-          </button>
-        </div> */}
       </nav>
     </>
   );
